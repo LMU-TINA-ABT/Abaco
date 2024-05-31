077f4cda4f0a278f1a9ae3d5a50c917830b686c1
anitba <tina.abt@gmx.net>
2024-05-31 22:33:12 +0200

    delete history

--- src/components/utils/DeleteButton.tsx
+++ src/components/utils/DeleteButton.tsx
@@ -1,13 +1,14 @@
 import {IconButton} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import {useDispatch} from "react-redux";
-import {colorAllPlaettchenGrey} from "../../store";
+import {colorAllPlaettchenGrey, setPlaettchenHistory} from "../../store";
 
 export const DeleteButton: React.FC<any> = (props: any) => {
     const dispatch = useDispatch();
 
     const handleClick = () => {
         dispatch(colorAllPlaettchenGrey());
+        dispatch(setPlaettchenHistory([]));
     }
 
     return (
